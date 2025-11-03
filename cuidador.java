087892e5eff81421fda36d57b1db04fcea46f8e4
NombreUsuario <correo@gmail.com>
2025-11-03 20:16:10 +0100

    cuidador hecho

--- cuidador.java
+++ cuidador.java
@@ -1,5 +1,60 @@
 package org.cuatrovientos.dam.psp.entregaTamagochi;
 
-public class cuidador {
+import java.util.ArrayList;
+import java.util.List;
 
+public class cuidador {
+    private List<Tamagochi> tamas;
+    private List<Thread> hilos;
+
+    public cuidador(int cantidad) {
+        tamas = new ArrayList<>();
+        hilos = new ArrayList<>();
+
+        for (int i = 0; i < cantidad; i++) {
+            Tamagochi t = new Tamagochi("Tama" + (i + 1));
+            tamas.add(t);
+            hilos.add(new Thread(t)); // cada Tamagotchi corre en su propio hilo
+        }
+    }
+
+    public void iniciar() {
+        for (Thread h : hilos) {
+            h.start();
+        }
+    }
+
+    public void mostrarEstados() {
+        for (Tamagochi t : tamas) {
+            t.estado();
+        }
+    }
+
+    public void alimentar(int indice) {
+        if (indiceValido(indice))
+            tamas.get(indice).alimentar();
+    }
+
+    public void limpiar(int indice) {
+        if (indiceValido(indice))
+            tamas.get(indice).limpiar();
+    }
+
+    public void jugar(int indice, int respuesta) {
+        if (indiceValido(indice))
+            tamas.get(indice).jugar(respuesta);
+    }
+
+    public void matar(int indice) {
+        if (indiceValido(indice))
+            tamas.get(indice).matar();
+    }
+
+    private boolean indiceValido(int i) {
+        return i >= 0 && i < tamas.size();
+    }
+
+    public int getCantidad() {
+        return tamas.size();
+    }
 }
